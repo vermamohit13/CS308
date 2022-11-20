16b0008aacf9139350baccab84c9e14a01df4b40
vermamohit13 <b20215@students.iitmandi.ac.in>
2022-11-20 23:07:11 +0600

    Added Auth routes, Message routes and socket connections

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 // Importing Required modules
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require ("cors");
+const cors = require("cors");
 const socket_io = require("socket.io");
 const authRoutes = require("./routes/auth");
-
+const messageRoutes = require("./routes/message");
 // Creating an express app
 const app = express();
 require("dotenv").config();
@@ -16,8 +16,8 @@ mongoose.connect(process.env.MongoDBURL,
         useUnifiedTopology: true,
     }).then(() => {
         console.log("Successfully connected to MongoDB");
-    }).catch((error) =>{
-         console.log(error.message);
+    }).catch((error) => {
+        console.log(error.message);
     })
 
 // Allowing Cross Origin Request
@@ -25,18 +25,29 @@ app.use(cors());
 app.use(express.json())
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 // Staring Server
-const server = app.listen(process.env.PORT, ()=> 
-                    console.log(`Server Listening on ${process.env.PORT}`));
+const server = app.listen(process.env.PORT, () =>
+    console.log(`Server Listening on ${process.env.PORT}`));
 
 const io = socket_io(server, {
     cors: {
-      origin: "http://localhost:3000",
-      credentials: true,
+        origin: "http://localhost:3000",
+        credentials: true,
     },
-  });
+});
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
 
-  io.on("connection", (socket_io) => {
-    console.log("Hello from server side");
-    console.log(socket.id);
-  })
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+        }
+    });
+});
