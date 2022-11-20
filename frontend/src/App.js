6b979d247e43087b9db0762599762dc668595fef
Kanishk Singla <kanishksingla23@gmail.com>
2022-11-20 18:59:32 +0530

    Added login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
-function App() {
+
+export default function App() {
   return (
- <>
- </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
