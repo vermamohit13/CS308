4c4d916be7ac03aa4ea86081b47fe61720c6a10a
VishwasGarg44 <vishwasgarg144@gmail.com>
2022-11-20 22:57:01 +0530

    Added API routes for login page.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,10 +2,21 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
 import { useNavigate, Link } from "react-router-dom";
+import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { loginRoute } from "../utils/APIRoutes";
 
 export default function Login() {
   const navigate = useNavigate();
   const [values, setValues] = useState({ username: "", password: "" });
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
   useEffect(() => {
     if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
       navigate("/");
@@ -17,11 +28,37 @@ export default function Login() {
   };
 
   const validateForm = () => {
-
+    const { username, password } = values;
+    if (username === "") {
+      toast.error("Email and Password is required.", toastOptions);
+      return false;
+    } else if (password === "") {
+      toast.error("Email and Password is required.", toastOptions);
+      return false;
+    }
     return true;
   };
 
   const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      const { username, password } = values;
+      const { data } = await axios.post(loginRoute, {
+        username,
+        password,
+      });
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem(
+          process.env.REACT_APP_LOCALHOST_KEY,
+          JSON.stringify(data.user)
+        );
+
+        navigate("/");
+      }
+    }
   };
 
   return (
@@ -29,7 +66,7 @@ export default function Login() {
       <FormContainer>
         <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
-            <img src="images/logo.svg" alt="logo" />
+            <img src={Logo} alt="logo" />
             <h1>snappy</h1>
           </div>
           <input
@@ -51,6 +88,7 @@ export default function Login() {
           </span>
         </form>
       </FormContainer>
+      <ToastContainer />
     </>
   );
 }
