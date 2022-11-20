6e7e75f6e60b188dec983aded92bcab4324d18ec
Kanishk Singla <kanishksingla23@gmail.com>
2022-11-20 19:16:28 +0530

    added emoji functionality

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -36,7 +36,7 @@ export default function Register() {
         <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
             <img src={Logo} alt="logo" />
-            <h1>snappy</h1>
+            <h1>Skyline</h1>
           </div>
           <input
             type="text"
