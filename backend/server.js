b777a2ade6c7b8f4411246647966aa35a969ebb4
vermamohit13 <b20215@students.iitmandi.ac.in>
2022-11-20 18:52:45 +0600

    Added Sockets in Server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,40 @@
 // Importing Required modules
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require ("cors");
+const socket_io = require("socket.io");
 
 // Creating an express app
 const app = express();
 require("dotenv").config();
 
+// Connecting to Mongo DB 
+mongoose.connect(process.env.MongoDBURL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log("Successfully connected to MongoDB");
+    }).catch((error) =>{
+         console.log(error.message);
+    })
+
+// Allowing Cross Origin Request
+app.use(cors());
+app.use(express.json())
+
 // Staring Server
-app.listen(process.env.PORT, ()=> 
+const server = app.listen(process.env.PORT, ()=> 
                     console.log(`Server Listening on ${process.env.PORT}`));
+
+const io = socket_io(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket_io) => {
+    console.log("Hello from server side");
+    console.log(socket.id);
+  })
